21d0f443b23a8eeaf6329c4137d335cbfad245ec
陈俊睿 <wb-cjr287265@alibaba-inc.com>
2017-07-07 15:58:24 +0800

    first commit

---
+++ src/routes/IndexPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Link } from 'dva/router'
+import styles from './IndexPage.css';
+
+function IndexPage({ dispatch }) {
+
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+      </ul>
+      <Link to="products">products</Link>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
