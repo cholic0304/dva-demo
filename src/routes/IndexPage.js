b0e7b376c2a325c3cd0c3b682948c60430aff99c
陈俊睿 <wb-cjr287265@alibaba-inc.com>
2018-02-25 18:44:00 +0800

    删除首页图片

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -10,7 +10,7 @@ function IndexPage({ dispatch }) {
         <h1 className={styles.title}>欢迎参观陈因为的玩具屋
           <span className={styles.rotate}>～</span>
         </h1>
-        <div className={styles.welcome} />
+        {/*<div className={styles.welcome} />*/}
         <div className={styles.list}>
           <div className={styles.item}><Link to="tetris">俄罗斯<br></br>方块</Link></div>
           <div className={styles.item}><Link to="tetris">俄罗斯<br></br>方块</Link></div>
